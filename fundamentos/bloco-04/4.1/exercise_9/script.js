c820c74bd42acf10cbaa9d80ed02e3d62b48e341
Lucas Ximenes <lucasdximenes@gmail.com>
2022-05-12 21:40:57 -0300

    add exercise_9 from 4.1

---
+++ fundamentos/bloco-04/4.1/exercise_9/script.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 7;
+const c = 4;
+
+if (a > b && a > c) {
+    console.log("O maior número é: " + a);
+} else if (b > a && b > c) {
+    console.log("O maior número é: " + b);
+} else {
+    console.log("O maior número é: " + c);
+}
