78211ea25d2578ef6857e07962b3112445a30086
Lucas Ximenes <lucasdximenes@gmail.com>
2022-05-12 21:40:19 -0300

    add exercise_5 from 4.1

---
+++ fundamentos/bloco-04/4.1/exercise_5/script.js
@@ -0,0 +1,15 @@
+var weekDay = "quarta-feira";
+
+if (
+    weekDay == "segunda-feira" ||
+    weekDay == "terça-feira" ||
+    weekDay == "quarta-feira" ||
+    weekDay == "quinta-feira" ||
+    weekDay == "sexta-feira"
+) {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else if (weekDay == "sabado" || weekDay == "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU");
+} else {
+    console.log("Dia da semana não reconhecido.");
+}
