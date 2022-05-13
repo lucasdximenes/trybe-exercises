54c899936cbd954d47b370d3cf815d5bcc3548e5
Lucas Ximenes <lucasdximenes@gmail.com>
2022-05-12 21:39:25 -0300

    add exercise_1 from 4.1

---
+++ fundamentos/bloco-04/4.1/exercise_1/script.js
@@ -0,0 +1,22 @@
+// #1
+const myName = "Lucas";
+
+// #2
+const birthCity = "Arcoverde";
+
+// #3
+let birthYear = 2002;
+
+// #4
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+// #5
+birthYear = 2030;
+console.log(birthYear);
+
+// #6
+birthCity = "Recife";
+console.log(birthCity);
+// Answer = Não posso reatribuir valor a uma constante
