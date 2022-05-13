f5b328ee257c51295a510f78f011738f1d32f480
Lucas Ximenes <lucasdximenes@gmail.com>
2022-05-12 21:40:28 -0300

    add exercise_6 from 4.1

---
+++ fundamentos/bloco-04/4.1/exercise_6/script.js
@@ -0,0 +1,19 @@
+let estadoPessoaCandidata = "aprovada";
+
+switch (estadoPessoaCandidata) {
+    case "aprovada":
+        console.log("Aprovada!");
+        break;
+
+    case "lista":
+        console.log("Lista de espera!");
+        break;
+
+    case "reprovada":
+        console.log("Reprovada!");
+        break;
+
+    default:
+        console.log("Não se aplica.");
+        break;
+}
