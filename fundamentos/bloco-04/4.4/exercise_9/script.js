fe9fd06260db986026624bec34c92fdeb258bf6e
Lucas Ximenes <lucasdximenes@gmail.com>
2022-05-18 13:44:32 -0300

    Added exercise_9 from 4.4

---
+++ fundamentos/bloco-04/4.4/exercise_9/script.js
@@ -0,0 +1,7 @@
+function higherNum(firstNum, secondNum) {
+    if (firstNum > secondNum) {
+        return firstNum + " é maior que " + secondNum;
+    } else {
+        return secondNum + " é maior que " + firstNum;
+    }
+}
