5ce56cbb199faa5385c053fcd6fd708c69eef2e4
Lucas Ximenes <lucasdximenes@gmail.com>
2022-05-18 13:44:32 -0300

    Added exercise_11 from 4.4

---
+++ fundamentos/bloco-04/4.4/exercise_11/script.js
@@ -0,0 +1,9 @@
+function positiveOrNegative(num) {
+    if (num > 0) {
+        return "positive";
+    } else if (num < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
