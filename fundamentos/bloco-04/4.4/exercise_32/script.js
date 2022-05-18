a8c528071060fb04fd84e6099a5b23b07593cd0f
Lucas Ximenes <lucasdximenes@gmail.com>
2022-05-18 13:44:33 -0300

    Added exercise_32 from 4.4

---
+++ fundamentos/bloco-04/4.4/exercise_32/script.js
@@ -0,0 +1,42 @@
+let moradores = {
+    blocoUm: [
+        {
+            nome: "Luiza",
+            sobrenome: "Guimarães",
+            andar: 10,
+            apartamento: 1005,
+        },
+        {
+            nome: "William",
+            sobrenome: "Albuquerque",
+            andar: 5,
+            apartamento: 502,
+        },
+    ],
+    blocoDois: [
+        {
+            nome: "Murilo",
+            sobrenome: "Ferraz",
+            andar: 8,
+            apartamento: 804,
+        },
+        {
+            nome: "Zoey",
+            sobrenome: "Brooks",
+            andar: 1,
+            apartamento: 101,
+        },
+    ],
+};
+
+for (let i = 0; i < moradores.blocoUm.length; i++) {
+    console.log(
+        moradores.blocoUm[i].nome + " " + moradores.blocoUm[i].sobrenome
+    );
+}
+
+for (let i = 0; i < moradores.blocoDois.length; i++) {
+    console.log(
+        moradores.blocoDois[i].nome + " " + moradores.blocoDois[i].sobrenome
+    );
+}
