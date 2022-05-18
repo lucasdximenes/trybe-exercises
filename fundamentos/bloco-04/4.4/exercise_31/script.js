3db575293599c9c71a826c97633023dc103170d3
Lucas Ximenes <lucasdximenes@gmail.com>
2022-05-18 13:44:33 -0300

    Added exercise_31 from 4.4

---
+++ fundamentos/bloco-04/4.4/exercise_31/script.js
@@ -0,0 +1,45 @@
+let moradores = {
+    blocoUm: [
+        {
+            nome: "Luiza",
+            sobrenome: "Guimarães",
+            andar: 10,
+            apartamento: 1005,
+        },
+        {
+            nome: "William",
+            sobrenome: "Albuquerque",
+            andar: 5,
+            apartamento: 502,
+        },
+    ],
+    blocoDois: [
+        {
+            nome: "Murilo",
+            sobrenome: "Ferraz",
+            andar: 8,
+            apartamento: 804,
+        },
+        {
+            nome: "Zoey",
+            sobrenome: "Brooks",
+            andar: 1,
+            apartamento: 101,
+        },
+    ],
+};
+
+let moradoresBlocoDois = moradores.blocoDois;
+let ultimoMoradorBlocoDois = moradoresBlocoDois[moradoresBlocoDois.length - 1];
+
+// Duas formas de se resolver
+console.log(
+    "O morador do bloco 2 de nome " +
+        ultimoMoradorBlocoDois.nome +
+        " " +
+        ultimoMoradorBlocoDois.sobrenome +
+        " mora no " +
+        ultimoMoradorBlocoDois.andar +
+        "° andar, apartamento " +
+        ultimoMoradorBlocoDois.apartamento
+);
