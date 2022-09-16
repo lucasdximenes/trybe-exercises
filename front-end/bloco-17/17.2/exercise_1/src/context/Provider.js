ba1c076d72d103bde6a78c862a936da50505c8ec
Lucas Ximenes <lucasdximenes@gmail.com>
2022-09-16 20:16:39 -0300

    add exercise_1 from 17.2

---
+++ front-end/bloco-17/17.2/exercise_1/src/context/Provider.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import CarsContext from "./CarsContext";
+
+const Provider = ({ children }) => {
+  const [cars, setCars] = useState({
+    redCar: false,
+    blueCar: false,
+    yellowCar: false,
+  });
+
+  const moveCar = (car, side) => {
+    setCars({
+      ...cars,
+      [car]: side,
+    });
+  };
+
+  const context = {
+    cars,
+    moveCar,
+  };
+
+  return (
+    <CarsContext.Provider value={context}>{children}</CarsContext.Provider>
+  );
+};
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
