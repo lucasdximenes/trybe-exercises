b31574bdee9623a88970ccb31bcbc0dfcdcd269f
Lucas Ximenes <lucasdximenes@gmail.com>
2022-10-12 16:59:26 -0300

    add exercise_5 from 19.3

---
+++ back-end/bloco-19/19.3/exercise_5/frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello From Dockerized React App</header>
+    </div>
+  );
+}
+
+export default App;
