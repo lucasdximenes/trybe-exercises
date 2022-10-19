b7e2174731ae4b6a0781540ae62e42df196c1c28
Lucas Ximenes <lucasdximenes@gmail.com>
2022-10-19 18:28:49 -0300

    add exercise_9 from 20.4

---
+++ back-end/bloco-20/20.4/exercises/exercise_9.sql
@@ -0,0 +1,10 @@
+/* 
+ Altere a classificação da tabela `BoxOffice` para 6.0 de todos os filmes que lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões no mercado interno.
+ */
+UPDATE
+  BoxOffice
+SET
+  rating = 6.0
+WHERE
+  international_sales < 300000000
+  AND domestic_sales > 200000000;
