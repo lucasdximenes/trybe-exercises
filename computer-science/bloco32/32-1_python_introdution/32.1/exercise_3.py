7fe39b32221e8937c96999c9b67ace464d3d843b
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-27 10:43:51 -0300

    Learning ./bloco32/32-1_python_introdution/32.1/exercise_3.py

---
+++ computer-science/bloco32/32-1_python_introdution/32.1/exercise_3.py
@@ -0,0 +1,16 @@
+'''
+Write a program that, given any value n, such that n > 1, prints on the screen a
+square made of asterisks of side length n. For example:
+
+n = 5
+
+*****
+*****
+*****
+*****
+*****
+'''
+
+n = 5
+for i in range(n):
+    print('*' * n)
