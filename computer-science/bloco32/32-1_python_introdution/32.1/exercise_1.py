a2d26d68975b9fe27dc8939b2c29b3811e52f89d
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-27 11:10:12 -0300

    Learning ./bloco32/32-1_python_introdution/32.1/exercise_1.py

---
+++ computer-science/bloco32/32-1_python_introdution/32.1/exercise_1.py
@@ -0,0 +1,10 @@
+# Create a function that receives two numbers and return the biggest one
+
+def biggest_number(number1, number2):
+    if number1 > number2:
+        return number1
+    else:
+        return number2
+
+
+print(biggest_number(2, 3))
